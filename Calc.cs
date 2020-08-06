db8af674ef81c54dd8d8cf733063ca75c8e34ed6
Kent Brian Richardt Pedersen (kent3211) <kent3211@zbc.dk>
2020-08-06 12:15:12 +0200

    Add project files.

---
+++ Calc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liste
+{
+    class Calc
+    {
+        public static List<int> AddNumbers(int amount, List<int> tempList)
+        {
+            for (int i = 0 + 1; i < amount + 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    tempList.Add(i);
+                }
+            }
+            return tempList;
+        }
+
+        public static List<int> RemoveThrees(List<int> tempList)
+        {
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                if (tempList[i] % 3 == 0)
+                {
+
+                    tempList.RemoveAt(i);
+
+                }
+            }
+            return tempList;
+        }
+
+        public static List<int> Add17(int seventeen, List<int> tempList)
+        {
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                if (i == 2)
+                {
+
+                    tempList.Insert(i, seventeen);
+
+                }
+            }
+            return tempList;
+        }
+
+
+        public static List<int> ReverseList(List<int> tempList, List<int> tempListNew)
+        {
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                int temp = tempList[i];
+                    tempListNew.Add(temp);
+
+            }
+            tempListNew.Reverse();
+            return tempListNew;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> straightNumbers = new List<int>();
+            List<int> straightNumbersReversed = new List<int>();
+
+            int numberCount = 20;
+
+            straightNumbers = Calc.AddNumbers(numberCount, straightNumbers);
+
+            for (int i = 0; i < straightNumbers.Count; i++)
+            {
+                Console.WriteLine(straightNumbers[i]);
+            }
+
+
+            straightNumbers = Calc.RemoveThrees(straightNumbers);
+            Console.WriteLine("numre minus 3");
+            for (int i = 0; i < straightNumbers.Count; i++)
+            {
+                Console.WriteLine(straightNumbers[i]);
+            }
+
+
+            int seventeen = 17;
+            straightNumbers = Calc.Add17(seventeen, straightNumbers);
+            Console.WriteLine("Nummer 17 tilføjet på plads 3");
+            for (int i = 0; i < straightNumbers.Count; i++)
+            {
+                Console.WriteLine(straightNumbers[i]);
+            }
+
+
+
+
+            straightNumbersReversed = Calc.ReverseList(straightNumbers, straightNumbersReversed);
+            Console.WriteLine("Nu er listen vendt om");
+            for (int i = 0; i < straightNumbersReversed.Count; i++)
+            {
+                Console.WriteLine(straightNumbersReversed[i]);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
